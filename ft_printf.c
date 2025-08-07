6b8cbe93c14025357a4c10d0c8ff7662cdd35b45
amy rodrigues <rmmgustavo@gmail.com>
2025-08-07 16:01:39 -0300

    melhorando codigo

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:47:56 by amyrodri          #+#    #+#             */
-/*   Updated: 2025/08/04 10:36:39 by amyrodri         ###   ########.fr       */
+/*   Updated: 2025/08/07 15:45:39 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,13 @@ static int	print_arg(va_list args, const char *format)
 		return (ft_putnbr(va_arg(args, int)));
 	if (*format == 'u')
 		return (ft_putuint(va_arg(args, unsigned int)));
-	if (*format == 'x' || *format == 'X')
-		return (ft_puthex_handle(va_arg(args, unsigned int), format));
+	if (*format == 'x')
+		return (ft_puthex(va_arg(args, unsigned int), "0123456789abcdef"));
+	if (*format == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), "0123456789ABCDEF"));
 	if (*format == '%')
 		return (ft_putchar('%'));
-	return (0);
+	return (-1);
 }
 
 int	ft_printf(const char *format, ...)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 11:56:28 by amyrodri          #+#    #+#             */
-/*   Updated: 2025/07/31 16:46:19 by amyrodri         ###   ########.fr       */
+/*   Updated: 2025/08/07 15:48:02 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,5 @@ int	ft_putstr(const char *s);
 int	ft_putnbr(int n);
 int	ft_putptr(void *p);
 int	ft_putuint(unsigned int nbr);
-int	ft_puthex_handle(unsigned int nbr, const char *format);
-
+int	ft_puthex(unsigned long n, char *base);
 #endif
